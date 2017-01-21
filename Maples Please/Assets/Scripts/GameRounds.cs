1ea0cbec7f12c8c7d1218942ae1610c521a4e6ba
Yury Korzun <yury@flyclops.com>
2017-01-21 14:36:36 -0500

    Updated game rounds

--- Maples Please/Assets/Scripts/GameRounds.cs
+++ Maples Please/Assets/Scripts/GameRounds.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 public class GameRounds : MonoBehaviour {
 	public GameRound[] Rounds;
 	private int _currentRound = 0;
-	private int _secondsElapsed = 0;
+	private float _secondsElapsed = 0f;
 
-	public HUDManager HudManager;
+	public HUDManager HUDManager;
 
 	private void Awake()
 	{
-
+		HUDManager.SetRound(CurrentRoundNumber);
 	}
 
 	private void Update()
 	{
-		
+		if (IsRoundCompleted())
+		{
+			_secondsElapsed = 0;
+			if (!IsGameCompleted())
+			{
+				_currentRound++;
+
+				HUDManager.SetRound(CurrentRoundNumber);
+			}
+		}
+
+		var roundedSeconds = Mathf.RoundToInt(_secondsElapsed);
+		HUDManager.SetSeconds(roundedSeconds);
+		_secondsElapsed += Time.deltaTime;
+	}
+
+	public int CurrentRoundNumber
+	{
+		get
+		{
+			return _currentRound + 1;
+		}
+	}
+
+	private bool IsRoundCompleted()
+	{
+		var currentRound = Rounds[_currentRound];
+
+		var isRoundCompleted = currentRound.LengthInSeconds <= _secondsElapsed;
+
+		return isRoundCompleted;
+	}
+
+	private bool IsGameCompleted()
+	{
+		var isCompleted = _currentRound >= Rounds.Length;
+
+		return isCompleted;
 	}
 }
