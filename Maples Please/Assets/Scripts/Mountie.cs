85be5575bbeaf8aed547a1174cf4609abcbc3638
BokononLives <jacob.heft@gmail.com>
2017-01-21 13:53:53 -0500

    mountie attack animations by click event

---
+++ Maples Please/Assets/Scripts/Mountie.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Mountie : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var animator = gameObject.GetComponent<Animator>();
+
+            var vectorFromMountieToMouse = Input.mousePosition - gameObject.transform.position;
+            var angle = Vector3.Angle(Vector3.up, vectorFromMountieToMouse);
+            if (0f <= angle && angle <= 22.5f)
+            {
+                animator.SetTrigger("ClickN");
+            }
+            else if (angle <= 45f)
+            {
+                animator.SetTrigger("ClickNNE");
+            }
+            else if (angle <= 67.5f)
+            {
+                animator.SetTrigger("ClickNE");
+            }
+            else
+            {
+                animator.SetTrigger("ClickENE");
+            }
+        }
+    }
+}
