0c9fc68637823275f8a2f23f572795f0f5bcd129
Yury Korzun <yury@flyclops.com>
2017-01-21 01:00:20 -0500

    Changes for playground

---
+++ Maples Please/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1 * Speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Maples Please/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+	public Enemy EnemyPrefab;
+	public GameObject Destination;
+	private List<Enemy> _enemyPool = new List<Enemy>();
+	private int _enemyPoolSize = 15;
+
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+		StartCoroutine(GenerateEnemies());
+	}
+
+	private IEnumerator GenerateEnemies()
+	{
+		var delay = Random.Range(3, 15);
+		yield return new WaitForSeconds(delay);
+
+		var numberOfEmenies = Random.Range(1, 5);
+
+		for(var i=0; i < numberOfEmenies; i++)
+		{
+			var position = Random.Range(-5.5f, 5.5f);
+			var speed = Random.Range(-1, -5);
+
+			var enemyPosition = new Vector3(position, transform.position.y, 0f);
+			var enemy = CreateEnemy(enemyPosition);
+			enemy.Speed = speed;
+		}
+
+		StartCoroutine(GenerateEnemies());
+	}
+
+	private Enemy CreateEnemy(Vector3 position)
+	{
+		var enemyInstance = _enemyPool.Where(x => !x.enabled).FirstOrDefault();
+		if (enemyInstance == null)
+		{
+			enemyInstance = Instantiate<Enemy>(EnemyPrefab, position, Quaternion.identity);
+
+			_enemyPool.Add(enemyInstance);
+		}
+
+		return enemyInstance;
+	}
+}
