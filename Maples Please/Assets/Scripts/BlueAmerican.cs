9506e62db6b0222fe074c2c5280acac0cd778798
BokononLives <jacob.heft@gmail.com>
2017-01-20 23:41:14 -0500

    AmericanGenerator prefab created

---
+++ Maples Please/Assets/Scripts/BlueAmerican.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BlueAmerican : MonoBehaviour
+{
+    public float Speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1 * Speed);
+    }
+}
