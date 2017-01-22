214ab79fc4509253d3e735013a536160af872492
Pimp-Town\Kenichi <ken.daskilewicz@outlook.com>
2017-01-22 11:13:29 -0500

    Leaf stuff

--- Maples Please/Assets/Scripts/Leaf.cs
+++ Maples Please/Assets/Scripts/Leaf.cs
@@ -2,11 +2,18 @@
 
 public class Leaf : Bullet
 {
+    public float RotationSpeed = 1;
+
     public override Bullet CloneInstance()
     {
         var instance = Instantiate(this, gameObject.transform.position, Quaternion.identity);
         instance.name = "Leaf";
         return instance;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, RotationSpeed));
     }
 
     void OnTriggerEnter2D(Collider2D coll)
