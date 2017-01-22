6a8a74e9f2e2379832ce3e6af5f65ca5763b8aae
Pimp-Town\Kenichi <ken.daskilewicz@outlook.com>
2017-01-21 20:44:30 -0500

    Projector script

---
+++ Maples Please/Assets/Scripts/Projector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Projector : MonoBehaviour {
+
+    private Collider2D _spotCollider;
+    private Sprite _leftSprite;
+    private Sprite _centerSprite;
+    private Sprite _rightSprite;
+
+    void Start ()
+    {
+        _spotCollider = GameObject.Find("Spot").GetComponent<Collider2D>();
+        _leftSprite = Resources.Load<Sprite>("SpotLeft");
+        _centerSprite = Resources.Load<Sprite>("SpotCenter");
+        _rightSprite = Resources.Load<Sprite>("SpotRight");
+    }
+	
+    void Update ()
+    {
+        var targetVector = _spotCollider.bounds.center - transform.position;
+        var angle = targetVector.ToAngleInDegrees();
+
+        GetComponent<SpriteRenderer>().sprite = angle >= 0 && angle < 60 ? _rightSprite : 
+                                                angle >= 50 && angle < 120 ? _centerSprite : 
+                                                _leftSprite;
+    }
+}
