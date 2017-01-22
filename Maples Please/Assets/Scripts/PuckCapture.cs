7a921a1073b287f97f3361e5c0c6eeaef3c40c0a
Yury Korzun <yury@flyclops.com>
2017-01-21 20:38:23 -0500

    Pucking puck!

---
+++ Maples Please/Assets/Scripts/PuckCapture.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckCapture : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.name == "Puck")
+		{
+			coll.gameObject.SetActive(false);
+		}
+	}
+}
