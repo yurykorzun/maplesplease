0ad255f40740243ba863e58111d48d1db66a82b6
Pimp-Town\Kenichi <ken.daskilewicz@outlook.com>
2017-01-22 14:34:24 -0500

    even more leaf updates

--- Maples Please/Assets/Scripts/Leaf.cs
+++ Maples Please/Assets/Scripts/Leaf.cs
@@ -3,6 +3,21 @@
 public class Leaf : Bullet
 {
     public float RotationSpeed = 1;
+    private Animator _animator;
+    private Rigidbody2D _rigidBody;
+    private Sprite _sprite;
+
+    private void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<Sprite>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, RotationSpeed));
+    }
 
     public override Bullet CloneInstance()
     {
@@ -11,18 +26,20 @@ public class Leaf : Bullet
         return instance;
     }
 
-    private void Update()
-    {
-        transform.Rotate(new Vector3(0, 0, RotationSpeed));
-    }
-
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.name == "Enemy")
         {
+            OnHit(coll);
             coll.gameObject.SetActive(false);
-
-            // todo - fancy syrup explosion here.
         }
+    }
+
+    private void OnHit(Collider2D coll)
+    {
+        _rigidBody.velocity = new Vector2(0, 0);
+        RotationSpeed = 0;
+        transform.localRotation = new Quaternion(0, 0, 0, 0);
+        _animator.SetTrigger("Land");
     }
 }
