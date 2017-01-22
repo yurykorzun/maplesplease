58f0b3f70e7b116a87acfb5182353d4554971bfa
Pimp-Town\Kenichi <ken.daskilewicz@outlook.com>
2017-01-21 22:10:26 -0500

    Projector Revisions

--- Maples Please/Assets/Scripts/Projector.cs
+++ Maples Please/Assets/Scripts/Projector.cs
@@ -2,10 +2,15 @@
 
 public class Projector : MonoBehaviour {
 
+    private const int FarRightAngleThreshold = -90;
+    private const int RightAngleThreshold = 15;
+    private const int CenterAngleThreshold = 60;
+    private const int LeftAngleThreshold = 120;
     private Collider2D _spotCollider;
     private Sprite _leftSprite;
     private Sprite _centerSprite;
     private Sprite _rightSprite;
+    private Sprite _farRightSprite;
 
     void Start ()
     {
@@ -13,6 +18,7 @@ public class Projector : MonoBehaviour {
         _leftSprite = Resources.Load<Sprite>("SpotLeft");
         _centerSprite = Resources.Load<Sprite>("SpotCenter");
         _rightSprite = Resources.Load<Sprite>("SpotRight");
+        _farRightSprite = Resources.Load<Sprite>("SpotFarRight");
     }
 	
     void Update ()
@@ -20,8 +26,10 @@ public class Projector : MonoBehaviour {
         var targetVector = _spotCollider.bounds.center - transform.position;
         var angle = targetVector.ToAngleInDegrees();
 
-        GetComponent<SpriteRenderer>().sprite = angle >= -90 && angle < 60 ? _rightSprite : 
-                                                angle >= 50 && angle < 120 ? _centerSprite : 
-                                                _leftSprite;
+        GetComponent<SpriteRenderer>().sprite = 
+            angle >= FarRightAngleThreshold && angle < RightAngleThreshold ? _farRightSprite :
+            angle >= RightAngleThreshold && angle < CenterAngleThreshold ? _rightSprite :
+            angle >= CenterAngleThreshold && angle < LeftAngleThreshold ? _centerSprite : 
+            _leftSprite;
     }
 }
